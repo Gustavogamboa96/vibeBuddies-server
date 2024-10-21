9134b377ed71d8983aeb49eff8fbdd3eca4e2ee2
LSierraOsorio <luis.asierraosorio99@gmail.com>
2024-10-21 10:11:14 -0700

    reverted

--- services/loginService.js
+++ services/loginService.js
@@ -15,8 +15,6 @@ async function login(username, password) {
         // data object that will be returned
         const data = {};
 
-        data.message = "breeeh, its the db"
-        return dataResponse(200, "success", data);
         // repository layer function call to get user by username
         const returnedUsers = await userDAO.getUserByUsername(username);
 
