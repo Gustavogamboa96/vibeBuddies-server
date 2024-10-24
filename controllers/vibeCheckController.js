dd1cc0fd582abf48bdc0a09a4095ba01d0354b34
Nick <93751758+kofflernick@users.noreply.github.com>
2024-10-24 09:41:30 -0400

    added functionality to create comments and delete comments

--- controllers/vibeCheckController.js
+++ controllers/vibeCheckController.js
@@ -1,136 +1,191 @@
-const express = require("express");
-const router = express.Router();
-const VibeCheckService = require("../services/vibeCheckService");
-
+const express = require("express")
+const router = express.Router()
+const VibeCheckService = require("../services/vibeCheckService")
 
 //post
-async function createVibeCheckController(req, res){
-    // const user_id = 123456;
-    const {user_id, username} = req.user;
-    const {album_id, review, rating} = req.body;
-    
-    try{
-        const response = await VibeCheckService.createVibeCheck(user_id, username, album_id, review, rating);
-        return res.status(response.httpStatus).json({
-            status: response.status,
-            ...(response.data && { data: response.data })
-        });
-    } catch(error) {
-        res.status(401).json({ message: error.message });
-      }
+async function createVibeCheckController(req, res) {
+  // const user_id = 123456;
+  const { user_id, username } = req.user
+  const { album_id, review, rating } = req.body
 
+  try {
+    const response = await VibeCheckService.createVibeCheck(
+      user_id,
+      username,
+      album_id,
+      review,
+      rating
+    )
+    return res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (error) {
+    res.status(401).json({ message: error.message })
+  }
+}
+
+//patch
+async function createCommentController(req, res) {
+  const { user_id, username } = req.user
+  const { comment_body, vibe_check_id } = req.body
+
+  try {
+    const response = await VibeCheckService.createComment(
+      user_id,
+      username,
+      vibe_check_id,
+      comment_body
+    )
+    res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (err) {
+    res.status(401).json({ message: err.message })
+  }
+}
+
+//delete
+async function deleteCommentController(req, res) {
+  const { user_id } = req.user
+  const { vibe_check_id, comment_id } = req.body
+
+  try {
+    const response = await VibeCheckService.removeComment(
+      user_id,
+      vibe_check_id,
+      comment_id
+    )
+    return res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (err) {
+    res.status(401).json({ message: err.message })
+  }
 }
 
 //get
 async function getAllVibeChecksController(req, res) {
-    // const user_id = 123456;
-    const user_id = req.user.user_id;
-    try{
-        const response = await VibeCheckService.getAllVibeChecks(user_id);
-        return res.status(response.httpStatus).json({
-            status: response.status,
-            ...(response.data && { data: response.data })
-        });
-    }catch(error){
-        res.status(401).json({message: error.message})
-    }
-
+  // const user_id = 123456;
+  const user_id = req.user.user_id
+  try {
+    const response = await VibeCheckService.getAllVibeChecks(user_id)
+    return res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (error) {
+    res.status(401).json({ message: error.message })
+  }
 }
 
 //delete
-async function deleteVibeCheckController(req, res){
-    // const user_id = 123456;
-    const user_id = req.user.user_id;
-    const vibe_check_id = req.params.id;
+async function deleteVibeCheckController(req, res) {
+  // const user_id = 123456;
+  const user_id = req.user.user_id
+  const vibe_check_id = req.params.id
 
-    try{
-        const response = await VibeCheckService.deleteVibeCheck(user_id, vibe_check_id);
-        return res.status(response.httpStatus).json({
-            status: response.status,
-            ...(response.data && { data: response.data })
-        });
-    }catch(error){
-        res.status(401).json({message: error.message})
-    }
-
-};
+  try {
+    const response = await VibeCheckService.deleteVibeCheck(
+      user_id,
+      vibe_check_id
+    )
+    return res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (error) {
+    res.status(401).json({ message: error.message })
+  }
+}
 
 //patch
-async function likeOrDislikeController(req, res){
-    // const user_id = 123456;
-    const {user_id, username} = req.user;
-    const vibe_check_id = req.params.id;
-    const type = req.params.likeordislike; 
+async function likeOrDislikeController(req, res) {
+  // const user_id = 123456;
+  const { user_id, username } = req.user
+  const vibe_check_id = req.params.id
+  const type = req.params.likeordislike
 
-    try{
-        const response = await VibeCheckService.likeOrDislike(user_id, username, vibe_check_id, type);
-        return res.status(response.httpStatus).json({
-            status: response.status,
-            ...(response.data && { data: response.data })
-        });
-    }catch(error){
-        res.status(401).json({message: error.message});
-    }
-
-};
-
-async function getVibeCheckByIdController(req, res){
-    const user_id = req.user.user_id;
-    const vibe_check_id = req.params.id;
-
-    try{
-        const response = await VibeCheckService.getVibeCheckById(user_id, vibe_check_id);
-        return res.status(response.httpStatus).json({
-            status: response.status,
-            ...(response.data && { data: response.data })
-        });
-    }catch(error){
-        res.status(401).json({message: error.message});
-    }
+  try {
+    const response = await VibeCheckService.likeOrDislike(
+      user_id,
+      username,
+      vibe_check_id,
+      type
+    )
+    return res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (error) {
+    res.status(401).json({ message: error.message })
+  }
 }
 
+async function getVibeCheckByIdController(req, res) {
+  const user_id = req.user.user_id
+  const vibe_check_id = req.params.id
 
-
-async function getVibeChecksByUserIdController(req, res){
-    const user_id = req.user.user_id;
-    const target_user_id = req.params.target_user_id;
-
-    try{
-        const response = await VibeCheckService.getVibeChecksByUserId(user_id, target_user_id);
-        return res.status(response.httpStatus).json({
-            status: response.status,
-            ...(response.data && { data: response.data })
-        });
-    }catch(error){
-        res.status(401).json({message: error.message});
-    }
-
+  try {
+    const response = await VibeCheckService.getVibeCheckById(
+      user_id,
+      vibe_check_id
+    )
+    return res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (error) {
+    res.status(401).json({ message: error.message })
+  }
 }
 
-async function getVibeChecksByUsernameController(req, res){
-    const user_id = req.user.user_id;
-    const target_username = req.params.target_username;
+async function getVibeChecksByUserIdController(req, res) {
+  const user_id = req.user.user_id
+  const target_user_id = req.params.target_user_id
 
-    try{
-        const response = await VibeCheckService.getVibeChecksByUsername(user_id, target_username);
-        return res.status(response.httpStatus).json({
-            status: response.status,
-            ...(response.data && { data: response.data })
-        });
-    }catch(error){
-        res.status(401).json({message: error.message});
-    }
-
+  try {
+    const response = await VibeCheckService.getVibeChecksByUserId(
+      user_id,
+      target_user_id
+    )
+    return res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (error) {
+    res.status(401).json({ message: error.message })
+  }
 }
 
-module.exports = {createVibeCheckController, 
-                  getVibeCheckByIdController,
-                  getAllVibeChecksController, 
-                  deleteVibeCheckController,
-                  likeOrDislikeController,
-                  getVibeChecksByUserIdController,
-                  getVibeChecksByUsernameController
-                }
+async function getVibeChecksByUsernameController(req, res) {
+  const user_id = req.user.user_id
+  const target_username = req.params.target_username
 
+  try {
+    const response = await VibeCheckService.getVibeChecksByUsername(
+      user_id,
+      target_username
+    )
+    return res.status(response.httpStatus).json({
+      status: response.status,
+      ...(response.data && { data: response.data }),
+    })
+  } catch (error) {
+    res.status(401).json({ message: error.message })
+  }
+}
 
-
+module.exports = {
+  createVibeCheckController,
+  getVibeCheckByIdController,
+  getAllVibeChecksController,
+  deleteVibeCheckController,
+  likeOrDislikeController,
+  getVibeChecksByUserIdController,
+  getVibeChecksByUsernameController,
+  createCommentController,
+  deleteCommentController,
+}
--- routes/vibeCheckRoute.js
+++ routes/vibeCheckRoute.js
@@ -1,21 +1,60 @@
-const express = require("express");
-const router = express.Router();
-const authenticateToken = require('../middleware/authenticateToken');
-const vibeCheckController = require("../controllers/vibeCheckController");
+const express = require("express")
+const router = express.Router()
+const authenticateToken = require("../middleware/authenticateToken")
+const vibeCheckController = require("../controllers/vibeCheckController")
 
+router.post(
+  "/",
+  authenticateToken,
+  vibeCheckController.createVibeCheckController
+)
 
-router.post("/", authenticateToken, vibeCheckController.createVibeCheckController)
+router.get(
+  "/",
+  authenticateToken,
+  vibeCheckController.getAllVibeChecksController
+)
 
-router.get("/", authenticateToken, vibeCheckController.getAllVibeChecksController);
+router.get(
+  "/:id",
+  authenticateToken,
+  vibeCheckController.getVibeCheckByIdController
+)
 
-router.get("/:id", authenticateToken, vibeCheckController.getVibeCheckByIdController);
+router.get(
+  "/users/:target_user_id",
+  authenticateToken,
+  vibeCheckController.getVibeChecksByUserIdController
+)
 
-router.get("/users/:target_user_id", authenticateToken, vibeCheckController.getVibeChecksByUserIdController);
+router.get(
+  "/username/:target_username",
+  authenticateToken,
+  vibeCheckController.getVibeChecksByUsernameController
+)
 
-router.get("/username/:target_username", authenticateToken, vibeCheckController.getVibeChecksByUsernameController);
+router.delete(
+  "/comments",
+  authenticateToken,
+  vibeCheckController.deleteCommentController
+)
 
-router.delete("/:id?", authenticateToken, vibeCheckController.deleteVibeCheckController);
+router.delete(
+  "/:id?",
+  authenticateToken,
+  vibeCheckController.deleteVibeCheckController
+)
 
-router.patch("/:id/:likeordislike?", authenticateToken, vibeCheckController.likeOrDislikeController);
+router.patch(
+  "/comments",
+  authenticateToken,
+  vibeCheckController.createCommentController
+)
 
-module.exports = router;
+router.patch(
+  "/:id/:likeordislike?",
+  authenticateToken,
+  vibeCheckController.likeOrDislikeController
+)
+
+module.exports = router
