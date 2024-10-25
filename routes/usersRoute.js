855148e8eb216013feefe266a670b1f1589d3b8b
Gustavo Gamboa Malaver <Gustavo.gamboa96@gmail.com>
2024-10-25 09:36:01 -0400

    Revert "Sprint4 gustavo"

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -6,15 +6,13 @@ const { updateProfile } = require("../controllers/updateProfileController");
 const { deleteAccount } = require("../controllers/deleteAccountController");
 const { getUserByUsername } = require("../controllers/getUserByUsernameController");
 const { getPersonalInformation } = require("../controllers/getPersonalInformationController")
-const { updatePasswordController } = require("../controllers/updatePasswordController");
 
 
 // middleware
 const { dataValidation } = require("../middleware/updateProfileDataValidation");
 const authenticateToken = require("../middleware/authenticateToken");
 
-//route to update password, expects body with currentPassword, newPassword
-router.patch("/password", authenticateToken, updatePasswordController);
+
 // route to update profile, expects body with info, protected route
 router.patch("", authenticateToken, dataValidation, updateProfile);
 // route to delete user, expects the userId as a route param
